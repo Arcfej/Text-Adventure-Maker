219f4edf81465a596540b93ee68d4e03f964765e
Arcfej <mayer.miklos91@gmail.com>
2023-03-18 12:30:45 +0000

    Style the creator page

--- text-adventure-creator/src/pages/Creator/Creator.tsx
+++ text-adventure-creator/src/pages/Creator/Creator.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import { onAuthStateChanged } from "firebase/auth";
 import {firebaseAuth} from "../../firebase/firebase-config";
 import {useNavigate} from "react-router-dom";
+import {Button, Stack, Typography} from "@mui/joy";
 
 function Creator(): JSX.Element {
     const navigate = useNavigate();
@@ -13,10 +14,14 @@ function Creator(): JSX.Element {
     }, [navigate]);
 
     return (
-        <div>
-            <div>Logged in</div>
-            <button onClick={() => firebaseAuth.signOut()}>Log out</button>
-        </div>
+        <Stack
+            spacing={2}
+            justifyContent="flex-start"
+            alignItems="center"
+        >
+            <Typography level="body1">Logged in</Typography>
+            <Button onClick={() => firebaseAuth.signOut()}>Log out</Button>
+        </Stack>
     );
 }
 
