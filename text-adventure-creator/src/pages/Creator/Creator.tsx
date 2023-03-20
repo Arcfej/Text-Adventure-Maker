6325bc465bb15ebc28771198b52b3ba4e32b6131
Arcfej <mayer.miklos91@gmail.com>
2023-03-20 17:28:48 +0000

    Add Firebase Functions to the project

--- text-adventure-creator/src/pages/Creator/Creator.tsx
+++ text-adventure-creator/src/pages/Creator/Creator.tsx
@@ -1,11 +1,26 @@
 import React, {useEffect} from 'react';
 import { onAuthStateChanged } from "firebase/auth";
-import {firebaseAuth} from "../../firebase/firebase-config";
+import {firebaseAuth, firebaseFunctions} from "../../firebase/firebase-config";
 import {useNavigate} from "react-router-dom";
 import {Button, Stack, Typography} from "@mui/joy";
+import {helloWorld} from "../../firebase/backendFunctions";
+import { connectFunctionsEmulator } from 'firebase/functions';
 
 function Creator(): JSX.Element {
     const navigate = useNavigate();
+    const [message, setMessage] = React.useState<string>("");
+
+    if (process.env.NODE_ENV === "development") {
+        connectFunctionsEmulator(firebaseFunctions, "localhost", 5001);
+    }
+
+    useEffect(() => {
+        const getMessage = async () => {
+            const {data: message} = await helloWorld();
+            setMessage(message);
+        };
+        getMessage();
+    }, []);
 
     useEffect(() => {
         onAuthStateChanged(firebaseAuth, (currentUser) => {
@@ -19,7 +34,7 @@ function Creator(): JSX.Element {
             justifyContent="flex-start"
             alignItems="center"
         >
-            <Typography level="body1">Logged in</Typography>
+            <Typography level="body1">{message}</Typography>
             <Button onClick={() => firebaseAuth.signOut()}>Log out</Button>
         </Stack>
     );
