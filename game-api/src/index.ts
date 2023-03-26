e6d16035ea5e80e97fadb64931f22042eeb7d0c6
Arcfej <mayer.miklos91@gmail.com>
2023-03-26 16:30:49 +0100

    Move game-api routes into a separate handler

--- game-api/src/index.ts
+++ game-api/src/index.ts
@@ -1,28 +1,7 @@
-// // Based on https://github.com/mongodb-developer/cloudflare-worker-rest-api-atlas
+// Based on https://github.com/mongodb-developer/cloudflare-worker-rest-api-atlas
 import { Router, IRequest} from 'itty-router';
+import gameApiHandler from "./game-api/game-api-handler";
 import * as utils from './utils';
-import * as Realm from "realm-web";
-
-const ObjectId = Realm.BSON.ObjectID;
-
-// Define type alias; available via `realm-web`
-type Document = globalThis.Realm.Services.MongoDB.Document;
-
-// Declare the interface for a Game document
-interface Game extends Document {
-		owner: string;
-		intro: string;
-}
-
-type GameCollection = globalThis.Realm.Services.MongoDB.MongoDBCollection<Game>;
-
-// declare a custom Request type to allow request injection from middleware
-type RequestWithGames = {
-		games: GameCollection;
-		uId: string;
-} & IRequest
-
-let app: Realm.App;
 
 export const withCorsPreflight = (request: IRequest) => {
 		if (request.method.toLowerCase() === 'options') {
@@ -32,88 +11,14 @@ export const withCorsPreflight = (request: IRequest) => {
 		}
 };
 
-// Middleware to connect to MongoDB Realm and append userId and games collection to request
-const withGames = async (request: IRequest, env: any) => {
-		try {
-				app = app || new Realm.App(env.REALM_APP_ID);
-				const credentials = Realm.Credentials.apiKey(env.REALM_API_KEY);
-				// Attempt to authenticate
-				const user: Realm.User = await app.logIn(credentials);
-				const client = user.mongoClient('mongodb-atlas');
-				request.uId = user.id;
-				request.games = client.db('games').collection<Game>('published');
-		} catch (err) {
-				console.log(err);
-				return utils.toError(500);
-		}
-};
-
-const getGame = async (request: RequestWithGames) => {
-		const game = await request.games.findOne({
-				_id: new ObjectId(request.params.id)
-		});
-		return game ? utils.reply(game) : utils.toError('Game not found', 404);
-};
-
-const getGames = async (request: RequestWithGames) => {
-		console.log('getGames');
-		const games = await request.games.find();
-		console.log(games);
-		const response = utils.reply(games);
-		console.log(response);
-		return response;
-};
-
-const postGame = async (request: RequestWithGames) => {
-		const {game, uId} = await request.json();
-		return utils.reply(await request.games.insertOne({
-				owner: uId,
-				intro: game
-		}));
-};
-
-const updateGame = async (request: RequestWithGames) => {
-		const {game} = await request.json();
-		const result = await request.games.updateOne(
-				{_id: new ObjectId(request.params.id)},
-				{$set: {intro: game}}
-		);
-		return result.matchedCount > 0
-				? utils.reply("Game updated")
-				: utils.toError('Game not found', 404);
-};
-
-const deleteGame = async (request: RequestWithGames) => {
-		const result = await request.games.deleteOne({
-				_id: new ObjectId(request.params.id)
-		});
-		return result.deletedCount > 0
-				? utils.reply("Game deleted")
-				: utils.toError('Game not found', 404);
-};
-
 const missingHandler = () => utils.toError('Method not allowed', 405);
 
 // Create a new router
 const router = Router();
 
 router
-		.all('*', withCorsPreflight, withGames)
-		.get('/games/:id', async (request: IRequest) => {
-				return await getGame(request as RequestWithGames);
-		})
-		.get('/games/', async (request: IRequest) => {
-				return await getGames(request as RequestWithGames);
-		})
-		.post('/games/', async (request: IRequest) => {
-				return await postGame(request as RequestWithGames);
-		})
-		.patch('/games/:id', async (request: IRequest) => {
-				return await updateGame(request as RequestWithGames);
-		})
-		.delete('/games/:id', async (request: IRequest) => {
-				return await deleteGame(request as RequestWithGames);
-		})
+		.all('*', withCorsPreflight)
+		.all('/games', gameApiHandler)
 		.all('*', missingHandler);
 
 export default {
