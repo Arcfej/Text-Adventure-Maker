ae230dedc37ab328d801f4a9584ea7d731437169
Arcfej <mayer.miklos91@gmail.com>
2023-03-30 05:37:56 +0100

    List published games on mobile app.

--- text-adventure-creator/src/pages/Creator/Creator.tsx
+++ text-adventure-creator/src/pages/Creator/Creator.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import { onAuthStateChanged } from "firebase/auth";
 import {firebaseAuth} from "../../firebase/firebase-config";
 import {useNavigate} from "react-router-dom";
@@ -6,8 +6,6 @@ import {Button, Stack} from "@mui/joy";
 
 function Creator(): JSX.Element {
     const navigate = useNavigate();
-    const [games, setGames] = useState<Array<{key: string, value: string}>>([]);
-    const [count, setCount] = useState(0);
 
     useEffect(() => {
         onAuthStateChanged(firebaseAuth, (currentUser) => {
@@ -15,36 +13,20 @@ function Creator(): JSX.Element {
         });
     }, [navigate]);
 
-    const fetchGames = async () =>
-        fetch("https://backend.text-adventure-maker.workers.dev/games",
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                }
-            })
-            .then(response => response.json())
-            .then(data => setGames(data))
-            .then(() => console.log(games));
-
-    useEffect(() => {
-        fetchGames();
-    }, []);
-
-    const insertGame = async () =>
-        fetch("https://backend.text-adventure-maker.workers.dev/games",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    game: count,
-                })
-            })
-            .then(response => response.json())
-            .then(() => setCount(count + 1))
-            .then(fetchGames);
+    // const insertGame = async () =>
+    //     fetch("https://backend.text-adventure-maker.workers.dev/games",
+    //         {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-Type": "application/json",
+    //             },
+    //             body: JSON.stringify({
+    //                 game: count,
+    //             })
+    //         })
+    //         .then(response => response.json())
+    //         .then(() => setCount(count + 1))
+    //         .then(fetchGames);
 
     return (
         <Stack
@@ -53,8 +35,6 @@ function Creator(): JSX.Element {
             alignItems="center"
         >
             <Button onClick={() => firebaseAuth.signOut()}>Log out</Button>
-            <Button onClick={fetchGames}>List / update games</Button>
-            <Button onClick={insertGame}>Insert game</Button>
         </Stack>
     );
 }
