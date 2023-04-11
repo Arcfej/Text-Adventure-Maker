af31cb971a47c5c84c36c8ceebdbeacab8ae00de
Arcfej <mayer.miklos91@gmail.com>
2023-04-11 14:41:49 +0100

    Update game listing with the new published format.

--- TextAdventurePlayer/App.tsx
+++ TextAdventurePlayer/App.tsx
@@ -1,54 +1,60 @@
 import React, {useEffect, useState} from 'react';
-import {Appbar, List} from 'react-native-paper';
+import {Appbar, Divider, Text} from 'react-native-paper';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {
-  FlatList,
-  ListRenderItemInfo,
-  SafeAreaView,
-  useColorScheme,
+    FlatList,
+    ListRenderItemInfo,
+    SafeAreaView,
+    useColorScheme,
+    View,
 } from 'react-native';
 
-interface Game {
-  intro: string;
-  owner: string;
-  _id: string;
+interface GameTitle {
+    _id: string;
+    title: string;
 }
 
 function App(): JSX.Element {
-  const [games, setGames] = useState<Game[]>([]);
-  const isDarkMode = useColorScheme() === 'dark';
+    const [gameTitles, setGameTitles] = useState<GameTitle[]>([]);
+    const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    };
 
-  const fetchGames = async () =>
-    fetch('https://backend.text-adventure-maker.workers.dev/games', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(response => response.json())
-      .then(data => setGames(data));
+    const fetchGames = async () => {
+        // fetch('http://localhost:8787/games', {
+        return fetch('https://backend.text-adventure-maker.workers.dev/games', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(data => setGameTitles(data.games));
+    };
 
-  useEffect(() => {
-    fetchGames();
-  }, []);
+    useEffect(() => {
+        fetchGames();
+    }, []);
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <Appbar.Header>
-        <Appbar.Content title="Games" />
-      </Appbar.Header>
-      <FlatList
-        data={games}
-        renderItem={({item}: ListRenderItemInfo<Game>) => (
-          <List.Item title={item.intro} />
-        )}
-      />
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <Appbar.Header>
+                <Appbar.Content title="Games"/>
+            </Appbar.Header>
+            <FlatList
+                data={gameTitles}
+                renderItem={({item}: ListRenderItemInfo<GameTitle>) => (
+                    <View style={{ flexDirection: 'column', alignItems: 'stretch', width: '100%'}}>
+                        <Text variant='titleLarge' style={{padding: 24}}>{item.title}</Text>
+                        <Divider style={{borderColor: 'white', borderWidth: .7}}/>
+                    </View>
+                )}
+                keyExtractor={item => item._id}
+            />
+        </SafeAreaView>
+    );
 }
 
 export default App;
